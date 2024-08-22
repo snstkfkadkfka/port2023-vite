e8b99d4e4834fa93510dabd90e5498b1bd6950b3
snstkfkadkfka <qotnrqks@naver.com>
2024-08-22 15:49:49 +0900

    😜 헤더 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,10 +1,11 @@
-const headerToggle=document.getElementById("headerToggle");
-const headerNav=document.querySelector(".header__nav");
-if(headerToggle){
-    headerToggle.addEventListener("click",()=>{
-        headerNav.classList.toggle("show");
-        headerToggle.getAttribute("aria-expanded")==="true"
-        ?headerToggle.setAttribute("aria-expanded","false")
-        :headerToggle.setAttribute("aria-expanded","frue");
+const headerToggle = document.getElementById("headerToggle");
+const headerNav = document.querySelector(".header__nav");
+
+if (headerToggle) {
+    headerToggle.addEventListener("click", () => {
+    headerNav.classList.toggle("show");
+    headerToggle.getAttribute("aria-expanded") === "true"
+        ? headerToggle.setAttribute("aria-expanded", "false")
+        : headerToggle.setAttribute("aria-expanded", "true");
     });
 }
